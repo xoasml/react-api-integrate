67bea4927258b5177a8d5fb00964233265a50660
TaeHoon <31303655+xoasml@users.noreply.github.com>
2023-12-27 00:35:17 +0900

    add: Context 비동기 작업 상태 관리

--- src/Users.js
+++ src/Users.js
@@ -1,24 +1,20 @@
 import React, {useState} from 'react';
-import axios from "axios";
-import {useAsync} from "react-async";
 import User from "./User";
+import {getUsers, useUsersDispatch, useUsersState} from "./UsersContext";
 
-
-async function getUsers() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
-    return response.data;
-}
 
 function Users() {
     const [userId, setUserId] = useState(null);
-    const {data: users, error, isLoading, reload, run} = useAsync({
-        deferFn: getUsers
-    })
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
 
-    // const {loading, data: users, error} = state;
-    if (isLoading) return <div>로딩중..</div>
+    const {loading, data: users, error} = state.users;
+    const fetchData = () => {
+        getUsers(dispatch);
+    }
+    if (loading) return <div>로딩중..</div>
     if (error) return <div>에러 발생</div>
-    if (!users) return <button onClick={run}>불러오기</button>
+    if (!users) return <button onClick={fetchData}>불러오기</button>
 
     return (
         <>
@@ -28,7 +24,7 @@ function Users() {
                         {user.username} ({user.name})
                     </li>)}
             </ul>
-            <button onClick={reload}>다시 불러오기</button>
+            <button onClick={fetchData}>다시 불러오기</button>
             {userId && <User id={userId}/>}
         </>
     );
