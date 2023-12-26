c58cef70f89fbb884839d82a4b6fb0a663ab7e86
TaeHoon <31303655+xoasml@users.noreply.github.com>
2023-12-26 19:18:03 +0900

    add : reducer 추가

---
+++ src/Users.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useReducer} from 'react';
+import axios from "axios";
+import {reducer} from "./AsyncReducer";
+
+function Users() {
+
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: null,
+        error: null,
+    });
+
+    const fetchUsers = async () => {
+        dispatch({type: 'LOADING'});
+        try {
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users/'
+            );
+            dispatch({type: 'SUCCESS', data: response.data});
+        } catch (e) {
+            dispatch({type: 'ERROR', error: e})
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const {loading, data: users, error} = state;
+    if (loading) return <div>로딩중..</div>
+    if (error) return <div>에러 발생</div>
+    if (!users) return null;
+
+    return (
+        <>
+            <ul>
+                {users.map(user =>
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>)}
+            </ul>
+            <button onClick={fetchUsers}>다시 불러오기</button>
+        </>
+    );
+}
+
+export default Users;
