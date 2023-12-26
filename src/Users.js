a3e1b50b6a2afe2a6c6631bf5240d00142ae1d1b
TaeHoon <31303655+xoasml@users.noreply.github.com>
2023-12-26 23:03:44 +0900

    add : 커스텀 훅 useAsync 구현

--- src/Users.js
+++ src/Users.js
@@ -1,45 +1,33 @@
-import React, {useEffect, useReducer} from 'react';
+import React, {useState} from 'react';
 import axios from "axios";
-import {reducer} from "./AsyncReducer";
+import useAsync from "./useAsync";
+import User from "./User";
+
+
+async function getUsers() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
+    return response.data;
+}
 
 function Users() {
 
-    const [state, dispatch] = useReducer(reducer, {
-        loading: false,
-        data: null,
-        error: null,
-    });
-
-    const fetchUsers = async () => {
-        dispatch({type: 'LOADING'});
-        try {
-            const response = await axios.get(
-                'https://jsonplaceholder.typicode.com/users/'
-            );
-            dispatch({type: 'SUCCESS', data: response.data});
-        } catch (e) {
-            dispatch({type: 'ERROR', error: e})
-        }
-    }
-
-    useEffect(() => {
-        fetchUsers();
-    }, []);
-
+    const [state, refetch] = useAsync(getUsers, [], true);
+    const [userId, setUserId] = useState(null);
     const {loading, data: users, error} = state;
     if (loading) return <div>로딩중..</div>
     if (error) return <div>에러 발생</div>
-    if (!users) return null;
+    if (!users) return <button onClick={refetch}>불러오기</button>
 
     return (
         <>
             <ul>
                 {users.map(user =>
-                    <li key={user.id}>
+                    <li key={user.id} onClick={() => setUserId(user.id)}>
                         {user.username} ({user.name})
                     </li>)}
             </ul>
-            <button onClick={fetchUsers}>다시 불러오기</button>
+            <button onClick={refetch}>다시 불러오기</button>
+            {userId && <User id={userId}/>}
         </>
     );
 }
